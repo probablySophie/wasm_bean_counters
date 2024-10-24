606c1978bfe983ff7a2df9fa2be550a6a1d1590f
ProbablySophie <70588905+probablySophie@users.noreply.github.com>
2024-10-24 16:12:02 +0800

    it doesn't default to 8001 :(

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 tmux clear-history; 
 clear; 
 if . build.sh; then
-	printf "${green}Successfully built.  Hosting on: http://127.0.0.1:8001${normal}\n"
+	printf "${green}Successfully built!${normal}\n"
 	http www -q
 else
 	printf "${red}Failed to build.  Not running web server${normal}\n"
