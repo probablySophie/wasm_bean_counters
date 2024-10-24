d8a5e83ddd82725fdcbdcae676d37ef7c29cccfa
ProbablySophie <70588905+probablySophie@users.noreply.github.com>
2024-10-24 14:06:53 +0800

    Better error reporting & no longer running http on failure

--- build.sh
+++ build.sh
@@ -1,28 +1,56 @@
 #!/bin/bash
 
 PKG_NAME="wasm_bean_counters" # TODO: figure out how to auto update this to our package name
+# TODO: Come through and have proper error messages
 
 # Build a non-release version
-cargo build --target wasm32-unknown-unknown
+if cargo build --target wasm32-unknown-unknown ; then 
+	echo "";
+else
+	printf "${red}Failed to build for target: wasm32-unknown-unknown${normal}\n"
+	return 1
+fi
 
 # do something???
 #wasm-bindgen --target web --no-typescript --out-dir target/wasm32-unknown-unknown/debug target/wasm32-unknown-unknown/debug/wasm_sample.wasm
-wasm-bindgen --target web --no-typescript --out-dir target/wasm32-unknown-unknown/debug "target/wasm32-unknown-unknown/debug/${PKG_NAME}.wasm"
+if wasm-bindgen --target web --no-typescript --out-dir target/wasm32-unknown-unknown/debug "target/wasm32-unknown-unknown/debug/${PKG_NAME}.wasm"; then
+	printf "";
+else
+	return 1
+fi
 
 # https://github.com/DelphinusLab/zkWasm/issues/145
 # ??????????????
 # wasm-opt -O3 target/wasm32-unknown-unknown/debug/wasm_sample_bg.wasm -o target/wasm32-unknown-unknown/debug/wasm_sample_bg.wasm --signext-lowering
-wasm-opt -O3 "target/wasm32-unknown-unknown/debug/${PKG_NAME}_bg.wasm" -o "target/wasm32-unknown-unknown/debug/${PKG_NAME}_bg.wasm" --signext-lowering
+if wasm-opt -O3 "target/wasm32-unknown-unknown/debug/${PKG_NAME}_bg.wasm" -o "target/wasm32-unknown-unknown/debug/${PKG_NAME}_bg.wasm" --signext-lowering ; then
+	printf "";
+else
+	printf "${red}????${normal}\n"
+	return 1
+fi
 
 
 # Shave down everything we don't need
-# wasm-gc target/wasm32-unknown-unknown/debug/wasm_sample.wasm
-# wasm-gc target/wasm32-unknown-unknown/debug/wasm_sample_bg.wasm
-wasm-gc "target/wasm32-unknown-unknown/debug/${PKG_NAME}_bg.wasm"
+if wasm-gc "target/wasm32-unknown-unknown/debug/${PKG_NAME}_bg.wasm"; then
+	printf ""
+else
+	printf "${red}Failed to run wasm-gc${normal}\n"
+	return 1
+fi
 
 # Copy the file we made into the www directory
 # cp target/wasm32-unknown-unknown/debug/wasm_sample.wasm www/
 # cp target/wasm32-unknown-unknown/debug/wasm_sample_bg.wasm www/
-cp "target/wasm32-unknown-unknown/debug/${PKG_NAME}_bg.wasm" www/
+if cp "target/wasm32-unknown-unknown/debug/${PKG_NAME}_bg.wasm" www/ ; then
+	printf ""
+else
+	printf "${red}Failed to copy main WASM file${normal}\n"
+	return 1
+fi
 
-cp "target/wasm32-unknown-unknown/debug/${PKG_NAME}.js" www/
+if cp "target/wasm32-unknown-unknown/debug/${PKG_NAME}.js" www/ ; then
+	printf ""
+else
+	printf "${red}Failed to copy WASM JS file${normal}\n"
+	return 1
+fi
--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 tmux clear-history; 
 clear; 
-. build.sh; 
-# TODO: Don't run if build.sh went wrong somewhere
-#python3 run_server.py;
-http www -q
+if . build.sh; then
+	printf "${green}Successfully built.  Hosting on: http://127.0.0.1:8001${normal}\n"
+	http www -q
+else
+	printf "${red}Failed to build.  Not running web server${normal}\n"
+fi
