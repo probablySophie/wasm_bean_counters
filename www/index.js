1c18a83b82b8d4a774c7e160b84b237020b2a881
ProbablySophie <70588905+probablySophie@users.noreply.github.com>
2024-10-26 20:42:26 +0800

    apparently date.now is evil!

--- www/index.js
+++ www/index.js
@@ -2,30 +2,36 @@ import init, { Game } from "./build/wasm_bean_counters.js"
 
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
-let lastFrame = Date.now();
+let lastFrame = performance.timeOrigin + performance.now();
 
 init().then(() => {
 	const game = Game.new();
 
-	canvas.addEventListener("mousemove", (event) => onMove(game, event));
+	canvas.addEventListener("mousemove", (event) => mouse_event(game, event));
+	canvas.addEventListener("mousedown", (event) => mouse_event(game, event));
 
 	requestAnimationFrame(() => frame(game));
 });
-
 function frame(game)
 {
-	const delta = Date.now() - lastFrame;
-	lastFrame = Date.now();
+	let now = (performance.timeOrigin + performance.now());
+	let delta = now - lastFrame;
+	lastFrame = now;
 
 	game.update(delta);
 	game.draw(context);
 	requestAnimationFrame(() => frame(game));
 }
 
-function onMove(game, event)
+function mouse_event(game, event)
 {
 	let rect = canvas.getBoundingClientRect();
 	let x = event.clientX - rect.left;
 	let y = event.clientY - rect.top;
-	game.mouse_moved(x, y);
+
+	switch (event.type)
+	{
+		case "mousemove": game.mouse_moved(x, y); break;
+		case "mousedown": game.mouse_pressed(x, y); break;
+	}
 }
